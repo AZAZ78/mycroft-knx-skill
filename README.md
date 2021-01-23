9a9953040fd2c0053eed512121fee9c20c2fccd1
AZAZ78 <alexander.zeh@gmx.de>
2021-01-23 21:01:29 +0100

    Sync

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/laptop-house.svg" card_color="#22A7F0" width="50" height="50" style="vertical-align:bottom"/> Knx
-Smart home integration of knx via eibnet/ip
+# <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/laptop-house.svg" card_color="#22A7F0" width="50" height="50" style="vertical-align:bottom"/> KNX
+Smart home integration of KNX via EIBNnet/IP
 
 ## About
 This skill let you control your smart home. it uses the pknx library from the the open-homeautomation project (https://github.com/open-homeautomation/pknx). control your lights, blinds and more ... simply with your voice!
 
 ## Examples
-* "Licht an"
-* "Licht aus"
-* "Licht ein"
+* "Turn the light on in the living room"
+* "Turn off the light in the kitchen"
+* "Shut down blinds for watching TV"
 
 ## Credits
-Alexander Zeh (AZAZ78)
+Alexander Zeh (@AZAZ78)
 
 ## Category
 **IoT**
@@ -22,3 +22,11 @@ Productivity
 #Eib
 #Smart home
 
+## Installation and Configuration
+tbd
+
+## Usage
+tbd
+
+## TODO
+tbd
--- __init__.py
+++ __init__.py
@@ -1,15 +1,176 @@
-from mycroft import MycroftSkill, intent_file_handler
+#Import mycroft integration
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import FallbackSkill
+from mycroft.util.format import nice_number
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
+from mycroft.util.log import getLogger
 
+#Import yaml reader for configuration
+import yaml
+from dataclasses import dataclass
+ 
+#Import pknx for communication to EIBNet/IP
+from knxip.ip import KNXIPTunnel
+from knxip.conversion import float_to_knx2, knx2_to_float, \
+    knx_to_time, time_to_knx, knx_to_date, date_to_knx, datetime_to_knx,\
+    knx_to_datetime
+from knxip.core import KNXException, parse_group_address
 
-class Knx(MycroftSkill):
+__author__ = 'azaz78'
+
+@dataclass
+class KnxValue:
+    value: int
+    valuetype: str = None
+
+class KnxSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        super().__init__(name="KnxSkill")
+        self._knx_tunnel = None
 
-    @intent_file_handler('knx.intent')
-    def handle_knx(self, message):
-        self.speak_dialog('knx')
+    def _setup(self, force=False):
+        self.log.debug ("setup skill (force: {})".format(force))
+        if self.settings is not None and (force or self._knx_tunnel is None):
+            ip = self.settings.get('host')
+            portnum = self.settings.get('portnum')
 
+            # Check if user filled host and portnum
+            if ip is None:
+                self.log.error ("host is not defined")
+                self.speak_dialog('knx.error.setup', data={
+                              "field": "host"})
+                return
+
+            try:
+                port = int(portnum)
+            except:
+                # String might be some rubbish (like '')
+                self.log.error ("portnum is not defined or invalid")
+                self.speak_dialog('knx.error.setup', data={
+                              "field": "portnum"})
+                return
+
+            self.log.info ("create new tunnel to {}:{}".format(ip, port))
+            self._knx_tunnel = KNXIPTunnel(ip, port)
+
+            self._light_entities = yaml.safe_load(self.settings.get('light'))
+            self._plug_entities = yaml.safe_load(self.settings.get('plug'))
+            self._blind_entities = yaml.safe_load(self.settings.get('blind'))
+            self._special_entities = yaml.safe_load(self.settings.get('special'))
+            self._actions = yaml.safe_load(self.settings.get('actions'))
+
+    def initialize(self):
+        self.language = self.config_core.get('lang')
+        #self.load_vocab_files(join(dirname(__file__), 'vocab', self.lang))
+
+        # Check and then monitor for credential changes
+        self.settings_change_callback = self.on_websettings_changed
+        self._setup()
+        
+    def on_websettings_changed(self):
+        # Force a setting refresh after the websettings changed
+        # Otherwise new settings will not be regarded
+        self._setup(True)
+
+    @intent_handler(IntentBuilder("LightIntent")
+                              .require("knx.light")
+                              .require("knx.action"))
+    def handle_knx_light(self, message):
+        self.log.info ("Handle light intent")
+        utterance = message.data.get('utterance')
+        self.log.info ("Utterance: {}".format(utterance))
+        self._handle_knx(utterance, self._light_entities, self._actions)
+
+    @intent_handler(IntentBuilder("BlindIntent")
+                              .require("knx.blind")
+                              .require("knx.action"))
+    def handle_knx_blind(self, message):
+        self.log.info ("Handle blind intent")
+        utterance = message.data.get('utterance')
+        self.log.info ("Utterance: {}".format(utterance))
+        self._handle_knx(utterance, self._blind_entities, self._actions)
+
+    @intent_handler(IntentBuilder("PlugIntent")
+                              .require("knx.plug")
+                              .require("knx.action"))
+    def handle_knx_plug(self, message):
+        self.log.info ("Handle plug intent")
+        utterance = message.data.get('utterance')
+        self.log.info ("Utterance: {}".format(utterance))
+        self._handle_knx(utterance, self._plug_entities, self._actions)
+
+    @intent_handler(IntentBuilder("SpecialIntent")
+                              .require("knx.special")
+                              .require("knx.action"))
+    def handle_knx_special(self, message):
+        self.log.info ("Handle special intent")
+        utterance = message.data.get('utterance')
+        self.log.info ("Utterance: {}".format(utterance))
+        self._handle_knx(utterance, self._special_entities, self._actions)
+
+    def _handle_knx(self, utterance, entities, actions):
+        target = self._get_target(utterance, entities)
+        value = self._get_value(utterance, actions)
+        if self._send_value(target, value) is True:
+           self.speak_dialog('knx.success')
+        else:
+           self.speak_dialog('knx.error')
+
+    def _send_value(self, target, value):
+        self.log.info ("Send {} to {}".format(value, target))
+        if target is None:
+           self.log.warning ("No target found")
+           return False
+        if value is None:
+           self.log.warning ("No action found")
+           return False
+        tunnel = self._knx_tunnel
+        try:
+           tunnel.connect()
+           if value.valuetype == None:
+              tunnel.group_write(parse_group_address(target), [value.value])
+           elif value.valuetype == "num":
+              tunnel.group_write(parse_group_address(target), [value.value], 1)
+           else:
+              self.log.warning ("No valid type found")
+           tunnel.disconnect()
+        except:
+           self.log.warning ("Error sending data")
+           return False
+        return True
+
+    def _get_target(self, text, entities):
+        default = None
+        for keys, target in entities.items():
+            for key in keys.split("|"):
+                if key in text:
+                    self.log.debug ("Found {} in {}".format(key, text))
+                    if isinstance(target, dict):
+                        return self._get_target (text, target)
+                    else:
+                        return target
+                if key == 'default':
+                    default = target
+        if default is not None:
+            self.log.debug ("Found no key but default")
+            if isinstance(default, dict):
+                return self._get_target (text, default)
+            else:
+                return default
+        return None
+
+    def _get_value(self, text, actions):
+        for key, value in actions.items():
+            if key in text:
+                val = value.split("|")
+                count = len(val)
+                if count == 1:
+                    return KnxValue(int(val[0]))
+                elif count == 2:
+                    return KnxValue(int(val[0]), val[1])
+        return None
 
 def create_skill():
-    return Knx()
+    return KnxSkill()
 
